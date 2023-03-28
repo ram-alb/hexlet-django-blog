56a44d1c206bf3aed4b29fd776a90c51c4eeedbe
Ramil Albakov <ramil.albakov2@gmail.com>
2023-03-28 20:59:02 +0600

    Add article app nad base template

---
+++ hexlet_django_blog/article/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'article/index.html', context={'app_name': 'Article-dramaticle'})
--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hexlet_django_blog',
+    'hexlet_django_blog.article',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
